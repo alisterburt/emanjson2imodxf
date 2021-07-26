c15bcb18897e95616dfa46ac9874846f55dc6a42
Alister Burt <alisterburt@gmail.com>
2021-07-26 12:47:37 +0100

    add test for tilt angles and example data

--- tests/test_emanjson2imodxf.py
+++ tests/test_emanjson2imodxf.py
@@ -7,11 +7,16 @@ from emanjson2imodxf import emanjson2imodxf
 TEST_DIRECTORY = Path(__file__).parent
 EMAN_JSON_FILE = TEST_DIRECTORY / 'example.json'
 IMOD_XF_FILE = TEST_DIRECTORY / 'example.xf'
+IMOD_TLT_FILE = TEST_DIRECTORY / 'example.tlt'
 
 
 def test_emanjson2imodxf():
-    """Check that output of emanjson2imodxf matches the expected xf file.
+    """Check that output matches expected xf and tlt files.
     """
-    xf = emanjson2imodxf(EMAN_JSON_FILE)
-    expected = np.loadtxt(IMOD_XF_FILE)
-    np.testing.assert_allclose(xf, expected, atol=1e-3)
+    xf, tilt_angles = emanjson2imodxf(EMAN_JSON_FILE)
+
+    expected_xf = np.loadtxt(IMOD_XF_FILE)
+    np.testing.assert_allclose(xf, expected_xf, atol=1e-3)
+
+    expected_tlt = np.loadtxt(IMOD_TLT_FILE)
+    np.testing.assert_allclose(tilt_angles, expected_tlt, atol=1e-2)
